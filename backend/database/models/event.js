3b4e544304f9aeea27bec88d773853b29eb7aeae
riaddaima <r.daima@aui.ma>
2022-11-24 21:37:20 +0100

    fix: changed type of startTime to be of type TIME

--- backend/database/models/event.js
+++ backend/database/models/event.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     startTime: {
-      type: DataTypes.DATE,
+      type: DataTypes.TIME,
       allowNull: false,
     },
     duration: {
