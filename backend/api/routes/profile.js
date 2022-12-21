0064253e5fbf3375fadc78ef97537b0ee1654803
RiadMAC <admin@RiadMAC.local>
2022-12-21 22:41:28 +0100

    added authorization header

--- backend/api/routes/profile.js
+++ backend/api/routes/profile.js
@@ -4,9 +4,9 @@ const profileRouter = express.Router();
 const { getAllProfiles, getProfileById, postNewProfile, updateProfileById } = require('../controller/profile');
 const { verify } = require('../middlewares/auth');
 
-profileRouter.get('/', getAllProfiles);
-profileRouter.get('/:id', getProfileById);
-profileRouter.post('/', postNewProfile);
+profileRouter.get('/', verify, getAllProfiles);
+profileRouter.get('/:id', verify, getProfileById);
+profileRouter.post('/', verify, postNewProfile);
 profileRouter.put('/:id', verify, updateProfileById);
 
 module.exports = profileRouter;
--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -4,9 +4,9 @@ const userRouter = express.Router();
 const { getAllUsers, getUserById, postNewUser, updateUserById, deleteUserById } = require('../controller/user');
 const { verify } = require('../middlewares/auth');
 
-userRouter.get('/', getAllUsers);
+userRouter.get('/', verify, getAllUsers);
 userRouter.get('/:id', verify, getUserById);
-userRouter.post('/', postNewUser);
+userRouter.post('/', verify, postNewUser);
 userRouter.put('/:id', verify, updateUserById);
 userRouter.delete('/:id', verify, deleteUserById);
 
