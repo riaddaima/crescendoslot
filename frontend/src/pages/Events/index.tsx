03d1774f83851712665041619cbcef0b81d67122
RiadMAC <admin@RiadMAC.local>
2022-12-22 17:40:15 +0100

    added autocomplete and book event button in frontend

--- frontend/src/pages/Events/index.tsx
+++ frontend/src/pages/Events/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box } from '@mui/material';
+import { Autocomplete, Box, Checkbox, TextField, Button } from '@mui/material';
 import Calendar from '../../components/Calendar';
 import Header from '../../components/Header';
 import SelectedEvents from '../../components/selectedEvents';
@@ -16,11 +16,22 @@ import {
 import { selectedEventsSelector } from '../../components/selectedEvents/reducer/selector';
 import Sidebar from '../../components/Sidebar/Sidebar';
 
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import { kidsSelector } from '../Dependents/reducer/selector';
+import { KidI } from '../Dependents/reducer/state';
+import { profileSelector } from '../../components/Profile/reducer/selector';
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
 const Events = () => {
   const dispatch = useAppDispatch();
   const selectedEvents = useAppSelector(selectedEventsSelector);
   const events = useAppSelector(calendarEventsSelector);
   const [eventsCopy, setEventsCopy] = useState(events);
+  const dependents = useAppSelector(kidsSelector);
+  const [selectedDependents, setSelectedDependents] = useState<KidI[]>([]);
+  const profile = useAppSelector(profileSelector);
 
   useEffect(() => {
     dispatch(getCalendarEvents());
@@ -44,6 +55,10 @@ const Events = () => {
     dispatch(deleteCalendarEvent(eventsCopy[index]));
   };
 
+  const exampleTest = () => {
+    console.log('The events the user', profile.userId, 'wants to book are the following', selectedEvents.events, 'and the selected dependents are', selectedDependents)
+  }
+
   return (
     <div>
       <Box sx={{ p: 3 }}>
@@ -60,9 +75,37 @@ const Events = () => {
             />
           </Box>
         </Box>
+        <Box sx={{ pt: 3 }}>
+          <Autocomplete
+            multiple
+            id="checkboxes-tags-demo"
+            fullWidth
+            options={dependents}
+            onChange={(event, value) => setSelectedDependents(value)}
+            disableCloseOnSelect
+            getOptionLabel={(option: KidI) => option.fname + ' ' + option.lname}
+            renderOption={(props, option, { selected }) => (
+              <li {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option.fname + ' ' + option.lname}
+              </li>
+            )}
+            renderInput={(params) => (
+              <TextField {...params} label="Dependents" placeholder="Dependent Name" />
+            )}
+          />
+        </Box>
+        <Box sx={{ pt: 3, pb: 3, float: 'right' }}>
+          <Button variant="contained" onClick={exampleTest}>Book Events</Button>
+        </Box>
       </Box>
     </div>
-    
+
   );
 }
 
